806f7f9a7d4c4916188ca50932a7a68d394c347b
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-10-18 16:08:04 -0300

    variables

---
+++ Variables.py
@@ -0,0 +1,32 @@
+nome = 'Mateus'
+idade = 26
+
+#print(idade, nome)
+
+altura = 1.77
+estudando = True
+
+#print(type(altura)) #saber o tipo da variável
+
+#!!Obtendo dados do user e salvando em variáveis!!
+
+nome = input('Qual é o seu nome?')
+
+print('Seu nome é', nome)
+
+
+
+"""
+EN
+1. int: Whole number, without the decimal part: 0, 5, -1, 1000.
+2. float: Real number, with decimal part: 1.0, -2.7, 3.14
+3. str: Textual data.
+4. Booleans: True or False.
+PT-BR
+1. int: N° inteiros, sem a parte decimal: 0, 5, -1, 1000.
+2. float: N° real, com a parte decimal: 1.0, -2.7, 3.14
+3. str: Dados textuais.
+4. booleanos: True or False.
+"""
+
+# > Modelo de fazer comentário
