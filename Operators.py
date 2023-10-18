a59f5fa27eb46f5c50f1affa0f439e96892e9fb9
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-10-18 16:18:36 -0300

    Operators

---
+++ Operators.py
@@ -0,0 +1,31 @@
+""""
+Soma: +
+Subtração: -
+Multiplicação: *
+Divisão: /
+Divisão inteira: //
+Resto  da divisão: %
+Potência: **
+"""
+numeroUm = 100
+numeroDois = 20
+
+#print(numeroUm / numeroDois)
+#print(numeroUm // numeroDois)
+#print(20 % 3)
+#print(4 ** 2)
+
+#Operadores booleanos
+
+idade1 = 10
+idade2 = 25
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2) # false
+print(idade1 <= idade2) # true
+print('Python' == 'python') # false
+print('Python' != 'abacaxi') # true
+print(altura1 >= altura2) # true
+print(altura2 > altura3) # false
